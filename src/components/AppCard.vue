3bfa983664fe329e8cca905f4d54a8dbe6ec6d41
Simone-Corso <scorso26@gmail.com>
2023-12-24 18:35:13 +0100

    ho tolto padding e modificsto una parte dello style

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="card">
-      <h3>{{ data.name }}</h3>
       <img :src="data.card_images[0].image_url" alt="Card Image" />
-      <p>{{ data.desc }}</p>
+      <h3>{{ data.name }}</h3>
+      <h2>Alien</h2>
     </div>
   </template>
   
@@ -20,14 +20,24 @@
   <style scoped>
   .card {
     border: 1px solid #ccc;
-    padding: 10px;
     margin: 10px;
     width: 150px; 
+    background-color: #D48F38;
   }
   
   .card img {
     max-width: 100%;
     height: auto;
   }
+
+h2{
+    text-align: center;
+  }
+
+  h3{
+    text-align: center;
+    color: white;
+  }
+  
   </style>
   
