a549b8850ad2a4fad606e58826428c5c85461da9
Simone-Corso <scorso26@gmail.com>
2023-12-24 18:43:30 +0100

    crea problemi per la seconda chiamata, verifico il codice.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class="container">
-  <div class="card-container">
-    <Card v-for="card in cards" :key="card.id" :data="card" />
+  <div>
+    <header>
+      
+      <select v-model="selectedArchetype" @change="filterByArchetype">
+        <option value="">Tutti gli Archetipi</option>
+        <option v-for="archetype in archetypes" :key="archetype" :value="archetype">{{ archetype }}</option>
+      </select>
+    </header>
+
+    <div class="container">
+      <div class="card-container">
+        <Card v-for="card in filteredCards" :key="card.id" :data="card" />
+      </div>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -17,10 +27,20 @@ export default {
   data() {
     return {
       cards: [],
+      archetypes: [],
+      selectedArchetype: '',
     };
+  },
+  computed: {
+    filteredCards() {
+      return this.selectedArchetype
+        ? this.cards.filter((card) => card.archetype === this.selectedArchetype)
+        : this.cards;
+    },
   },
   created() {
     this.fetchCards();
+    this.fetchArchetypes();
   },
   methods: {
     async fetchCards() {
@@ -29,6 +49,30 @@ export default {
         this.cards = response.data.data;
       } catch (error) {
         console.error('Error fetching cards:', error);
+      }
+    },
+    async fetchArchetypes() {
+      try {
+        const response = await axios.get('https://db.ygoprodeck.com/api/v7/archetypes.php');
+        this.archetypes = response.data.data;
+      } catch (error) {
+        console.error('Error fetching archetypes:', error);
+      }
+    },
+    filterByArchetype() {
+      // 
+      const url = this.selectedArchetype
+        ? `https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0&archetype=${this.selectedArchetype}`
+        : 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0';
+
+      this.fetchCardsWithArchetype(url);
+    },
+    async fetchCardsWithArchetype(url) {
+      try {
+        const response = await axios.get(url);
+        this.cards = response.data.data;
+      } catch (error) {
+        console.error('Error fetching cards with archetype:', error);
       }
     },
   },
@@ -54,4 +98,5 @@ body{
   justify-content: center;
 }
 </style>
+
 
