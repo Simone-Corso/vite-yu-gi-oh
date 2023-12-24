337a59cc88a3d249b4710feda51178f805f70c28
Simone-Corso <scorso26@gmail.com>
2023-12-24 18:01:26 +0100

    messo in ordine i file.

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+
+<template>
+  <div>
+    <AppHeader />
+    <AppMain />
+    <AppSearch  />
+  </div>
+</template>
+
+<script>
+import AppSearch from './components/AppSearch.vue';
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+
+export default {
+  data() {
+    return {
+      
+    };
+  },
+  methods: {
+    },
+  },
+  
+  components: {
+  },
+  created() {
+    
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,17 @@
+<template lang="">
+    <div>
+        <article>
+        </article>
+    </div>
+</template>
+<script>
+export default {
+    name:,
+
+    props: {
+    }
+}
+</script>
+<style lang="">
+    
+</style>
